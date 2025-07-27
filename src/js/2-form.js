9dfdb788ee43144f900d36251343bcc559a13e43
SERHII STETSIUK <skystets@gmail.com>
2025-07-27 14:30:40 +0200

    Finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log('Form');
+
+const formEl = document.querySelector('.feedback-form');
+
+let formData = {
+	email: '',
+	message: '',
+};
+
+
+
+formEl.addEventListener('input', e => {
+	const email = e.currentTarget.elements.email.value.trim();
+	const message = e.currentTarget.elements.message.value.trim();	
+	formData.email = email;
+	formData.message = message;
+	saveToLs('feedback-form-state', formData);
+	
+});
+function saveToLs(key, value) {
+	const jsonData = JSON.stringify(value);
+	localStorage.setItem(key, jsonData);
+}
+function getFromLs(key, defaultValue) {
+	const jsonData = localStorage.getItem(key);
+	try {
+		const data = JSON.parse(jsonData);
+		return data;
+	} catch {
+		return defaultValue || jsonData;
+	}
+
+};
+
+	const formLoaded = getFromLs('feedback-form-state');
+	
+	try {
+		formEl.elements.email.value.trim() = formLoaded.email;
+		formEl.elements.message.value.trim() = formLoaded.message;
+		formData = formLoaded;
+	} catch { };
+
+formEl.addEventListener('submit', e => {
+	e.preventDefault();
+	if (formEl.elements.email.value === '' || formEl.elements.email.value === '') { alert('Fill please all fields') }
+	else{console.log(formData)};
+	localStorage.removeItem('feedback-form-state');
+});
+
