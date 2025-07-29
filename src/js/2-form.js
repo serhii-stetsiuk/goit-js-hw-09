494fa258bdbfce8882362ef263318c676c0bca54
SERHII STETSIUK <skystets@gmail.com>
2025-07-29 13:13:36 +0200

    Fantastic hw

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,46 +6,52 @@ let formData = {
 	message: '',
 };
 const formEl = document.querySelector('.feedback-form');
-const formLoaded = getFromLs('feedback-form-state');
-	
-	try {
+const formLoaded = getFromLs('feedback-form-state', {});
+
+try {
+		
 		formEl.elements.email.value = formLoaded.email;
 		formEl.elements.message.value = formLoaded.message;
 		formData = formLoaded;
-	} catch { };
+} catch { };
+
 
 formEl.addEventListener('input', e => {
 	const email = e.currentTarget.elements.email.value.trim();
+	const message = e.currentTarget.elements.message.value.trim();
+	if (email === '' || message === '') { return; }
 	formData.email = email;
-	
-	saveToLs('feedback-form-state', formData);
-	
-});
-formEl.addEventListener('input', e => {
-	const message = e.currentTarget.elements.message.value.trim();	
 	formData.message = message;
 	saveToLs('feedback-form-state', formData);
-	
+
 });
 function saveToLs(key, value) {
 	const jsonData = JSON.stringify(value);
 	localStorage.setItem(key, jsonData);
 }
-function getFromLs(key, defaultValue) {
+
+
+
+function getFromLs(key) {
 	const jsonData = localStorage.getItem(key);
+	if (!jsonData || jsonData === 'null') { return; }
 	try {
 		const data = JSON.parse(jsonData);
 		return data;
 	} catch {
-		return defaultValue || jsonData;
+		return  jsonData;
 	}
+}
 
-};
 formEl.addEventListener('submit', e => {
 	e.preventDefault();
-	if (formEl.elements.email.value === '' || formEl.elements.message.value === '') { alert('Fill please all fields'); return felse; }
+	if (formEl.elements.email.value.trim() === '' || formEl.elements.message.value.trim() === '') { alert('Fill please all fields'); return; }
 	else{console.log(formData)};
 	localStorage.removeItem('feedback-form-state');
 	formEl.reset();
-});
+	formData.email = '';
+	formData.message = '';
+	
+	
+})
 
